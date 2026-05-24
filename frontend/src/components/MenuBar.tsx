104897914e0d3d05ae20d1caa4229b8975ff23be
NghiaNT1 <nghiant1@fpt.com>
2026-05-24 23:15:53 +0900

    remove: markdown editor title from menu bar

--- frontend/src/components/MenuBar.tsx
+++ frontend/src/components/MenuBar.tsx
@@ -1,4 +1,3 @@
-import { FileText } from 'lucide-react';
 import './MenuBar.css';
 
 interface MenuBarProps {}
@@ -6,10 +5,6 @@ interface MenuBarProps {}
 const MenuBar: React.FC<MenuBarProps> = () => {
   return (
     <div className="menu-bar">
-      <div className="menu-logo">
-        <FileText size={16} />
-        <span>Markdown Editor</span>
-      </div>
       <div className="menu-actions"></div>
     </div>
   );
